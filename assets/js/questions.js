964455a7995b15335fe7a9ba51c529308014ac47
Brandon Mellus <mellusbrandon@gmail.com>
2021-06-19 18:21:49 -0400

    Create questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+// An array of question objects
+//  question: The question string
+//  choices: The available answer choices (4 choices)
+//  answer:  The numbered index of the correct answer
+const questions = [
+    {
+        question: "Commonly used Data Types DO NOT include",
+        choices: [
+            "String",
+            "Number",
+            "Chonk",
+            "Boolean"
+        ],
+        answer: 2
+    },
+
+    {
+        question: "Which creates a read-only variable when declared before a variable name?",
+        choices: [
+            "const",
+            "let",
+            "var",
+            "wumbo"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "Which of the following is not a Web API",
+        choices: [
+            "console",
+            "DOM",
+            "Geolocation",
+            "PayPal"
+        ],
+        answer: 3
+    },
+
+    {
+        question: "What does the `%` operator do?",
+        choices: [
+            "Returns the first number percentage of the second number",
+            "Returns a boolean indicating if they are approximately equal",
+            "Returns the remainder of the division of the first and second number",
+            "No such operator exists in JavaScript"
+        ],
+        answer: 2
+    },
+    {
+        question: "How would you select an element in the DOM by its ID?",
+        choice: [
+            "document.fetchById",
+            "document.getElementById",
+            "element.getElementById",
+            "document.element.getById"
+        ],
+        answer: 1
+    }
+];
